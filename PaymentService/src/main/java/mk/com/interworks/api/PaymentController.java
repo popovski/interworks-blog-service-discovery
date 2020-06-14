352b1a352d2afe9f772df1a06e1da2141a014e86
Nikola Popovski <popovski.nikola@gmail.com>
2020-06-14 23:26:19 +0200

    Initial commit of all the projects

---
+++ PaymentService/src/main/java/mk/com/interworks/api/PaymentController.java
@@ -0,0 +1,38 @@
+package mk.com.interworks.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+@RequestMapping("/")
+public class PaymentController {
+	@Value("${service.instance.name:payment-service}")
+	private String instanceName;
+	
+	@Autowired
+	private EurekaClient client;
+	
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+	
+	@GetMapping
+	public String message() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = client.getNextServerFromEureka("product-service", false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		
+		ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+		
+		return response.getBody();
+	}
+}
